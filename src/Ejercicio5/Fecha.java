a45dd0a925a8a4f797a3871d844a8c5882249d24
Elton Lucas Ravina Agrelo <elton.ravina@mercadolibre.com>
2021-05-30 14:05:31 -0300

    Ejercicio 5 Fechas

--- src/Ejercicio5/Fecha.java
+++ src/Ejercicio5/Fecha.java
@@ -1,5 +1,6 @@
 package Ejercicio5;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -10,19 +11,21 @@ public class Fecha {
     private Date date;
 
     //METODOS
-    public boolean VerificarFecha(GregorianCalendar fecha){
-
-        return true;
+    public Date VerificarFecha(GregorianCalendar calendar){
+        return new Date();
     }
 
-    public void SumarDiaValorActualFecha(){
-
+    public Date SumarDia(GregorianCalendar calendar ,int day){
+        int actual = calendar.get(Calendar.DATE);//obtengo dia de la fecha
+        Date date = new Date(); //creamos una fecha nueva a retornar
+        date.setDate(actual+day); //le seteamos el dia actual + la cantidad de dias a sumar
+        return date; // retornamos la fecha nueva con los dias sumados.
     }
 
     //CONSTRUCTORES
-    public Fecha(GregorianCalendar calendar, Date date) {
+    public Fecha(GregorianCalendar calendar,Date date) {
         this.fecha = calendar;
-        this.date = date;
+        this.date =date;
     }
 
     public Fecha(Date date){
@@ -44,8 +47,6 @@ public class Fecha {
 
     @Override
     public String toString() {
-        return "Fecha{" +
-                "fecha=" + fecha +
-                '}';
+        return "Fecha --> "  + this.getDate() ;
     }
 }
--- src/Ejercicio5/mainFecha.java
+++ src/Ejercicio5/mainFecha.java
@@ -5,9 +5,15 @@ import java.util.GregorianCalendar;
 
 public class mainFecha {
     public static void main(String[] args) {
-        GregorianCalendar calendar =  new GregorianCalendar();
-        calendar.setTime(new Date());
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.set(2019,11,11);
+        Date date = calendar.getTime(); //creo la fecha que me indican en el Gregorian calendar.
 
-        System.out.println(calendar.getTimeZone().toZoneId());
+        Fecha fecha = new Fecha(date); //creamos una instancia de Fecha con la fecha del calendario.
+        Date newDate = fecha.SumarDia(calendar,5); //Creamos una nueva fecha con los dias sumados
+
+        fecha.setDate(newDate); //seteamos la nueva fecha
+
+        System.out.println(fecha.toString());
     }
 }
